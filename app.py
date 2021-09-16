c1ccd7a282c9009bd22001d2d33d606366c67cec
emsknock <25252659+emsknock@users.noreply.github.com>
2021-09-16 13:50:21 +0300

    Add db connection

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
