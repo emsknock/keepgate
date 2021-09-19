cd7f44e3986e25a475d208c4e5c1568e18669b65
emsknock <25252659+emsknock@users.noreply.github.com>
2021-09-19 17:18:37 +0300

    Move routes and db to their own modules

--- app.py
+++ app.py
@@ -4,13 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
-
-@app.route("/")
-def index():
-    return render_template("index.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
