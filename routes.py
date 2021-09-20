694564e6efb2ab306977ddf3c3a8d021b8684c2b
emsknock <25252659+emsknock@users.noreply.github.com>
2021-09-20 13:05:58 +0300

    Make /login GETable

--- routes.py
+++ routes.py
@@ -5,7 +5,7 @@ from flask import render_template
 def index():
     return render_template("index.html")
 
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     return ""
 
