0387db90d65f7622d26c06ae29db3811f75d6a79
emsknock <25252659+emsknock@users.noreply.github.com>
2021-09-20 12:56:35 +0300

    Create initial routes

--- routes.py
+++ routes.py
@@ -4,3 +4,31 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    return ""
+
+@app.route("/logout")
+def logout():
+    return ""
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return ""
+
+@app.route("/event/<id>", methods=["GET", "DELETE"])
+def event():
+    return ""
+
+@app.route("/event", methods=["POST"])
+def new_event():
+    return ""
+
+@app.route("/ticket/<id>", methods=["GET", "DELETE"])
+def ticket():
+    return ""
+
+@app.route("/ticket", methods=["POST"])
+def new_ticket():
+    return ""
