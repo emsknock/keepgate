fbaa6ca2e543ee8c3b1895a9978ebae438b8336a
emsknock <25252659+emsknock@users.noreply.github.com>
2021-09-20 13:26:45 +0300

    Create initial schema

---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    passhash TEXT
+);
+
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    title TEXT,
+    date TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE tickets (
+    id SERIAL PRIMARY KEY,
+    event_id INTEGER REFERENCES events,
+    holder TEXT,
+    extra_info TEXT
+)
