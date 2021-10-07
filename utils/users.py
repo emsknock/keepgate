58b8fc696a0de5b992fc5c2ace8c2f9c23f89a8f
emsknock <25252659+emsknock@users.noreply.github.com>
2021-10-07 19:48:32 +0300

    Move user logic into own module

---
+++ utils/users.py
@@ -0,0 +1,22 @@
+from db import exec, commit
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def new_user(username, password):
+    exec(
+        "INSERT INTO users (username, passhash) VALUES (:username, :passhash)",
+        {
+            "username": username,
+            "passhash": generate_password_hash(password)
+        }
+    )
+    commit()
+
+def check_login(username, password):
+    result = exec(
+        "SELECT id, password FROM users WHERE username=:username",
+        {
+            "username": username
+        }
+    )
+    user = result.fetchone()
+    return user and check_password_hash(user.passhash, password)
