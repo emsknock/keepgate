b4043a601d40188939db32ecd44f7e57ab83a481
emsknock <25252659+emsknock@users.noreply.github.com>
2021-10-11 17:04:50 +0300

    Return id on ticket creation

--- utils/tickets.py
+++ utils/tickets.py
@@ -2,10 +2,11 @@ from db import exec, commit
 from uuid import uuid4
 
 def new_ticket(event_id, user_id = None, extra_info = None):
-    exec(
+    id = exec(
         """
         INSERT INTO tickets (id, event_id, user_id, extra_info)
         VALUES (:id, :event_id, :user_id, :extra_info)
+        RETURNING id
         """,
         {
             "id": uuid4(),
@@ -15,6 +16,7 @@ def new_ticket(event_id, user_id = None, extra_info = None):
         }
     )
     commit()
+    return id.fetchone()[0]
 
 def get_ticket_count_for_event(event_id):
     result = exec(
