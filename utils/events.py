893b37be1f67be3aff9deb6642c8759753a9ca2d
emsknock <25252659+emsknock@users.noreply.github.com>
2021-10-11 16:49:52 +0300

    Rewrite event list query

--- utils/events.py
+++ utils/events.py
@@ -21,10 +21,11 @@ def get_detailed_event_list(user_id):
     result = exec(
         """
         SELECT e.id, e.title, e.date, e.extra_info,
-               COUNT(t.id) as ticket_count
-        FROM events e LEFT JOIN tickets t ON e.id = t.event_id
+               (SELECT COUNT(*) FROM tickets WHERE event_id = e.id) as ticket_count,
+               (SELECT COUNT(*) FROM passes WHERE event_id = e.id) as pass_count,
+               (SELECT COUNT(*) FROM organisers WHERE event_id = e.id) as organiser_count
+        FROM events e
         WHERE e.user_id = :user_id
-        GROUP BY e.id
         """,
         {
             "user_id": user_id
