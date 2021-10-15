44c02699b507753fa817da4e2af60481506c16bc
emsknock <25252659+emsknock@users.noreply.github.com>
2021-10-15 18:06:30 +0300

    Regroup

--- utils/users.py
+++ utils/users.py
@@ -4,22 +4,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from functools import wraps
 from flask import g, request, redirect, url_for, session
 
-def requires_signin(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if "username" in session and session["username"] != "":
-            return f(*args, **kwargs)
-        else:
-            return redirect("/")
-    return decorated_function
-
-def is_signed_in(session): return (
-    "username" in session and
-    "user_id" in session and
-    session["username"] != "" and
-    session["user_id"] != ""
-)
-
 def new_user(username, password):
     exec(
         "INSERT INTO users (username, passhash) VALUES (:username, :passhash)",
@@ -58,6 +42,22 @@ def is_username_free(username):
     )
     user = result.fetchone()
     return not user
+
+def requires_signin(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "username" in session and session["username"] != "":
+            return f(*args, **kwargs)
+        else:
+            return redirect("/")
+    return decorated_function
+
+def is_signed_in(session): return (
+    "username" in session and
+    "user_id" in session and
+    session["username"] != "" and
+    session["user_id"] != ""
+)
 
 def signin(username, session):
     session["username"] = username
