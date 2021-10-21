aff0bb83f7563807f01cc28e86ac45ffec4951a4
emsknock <25252659+emsknock@users.noreply.github.com>
2021-10-21 19:20:58 +0300

    Add routes for event management

--- routes/event.py
+++ routes/event.py
@@ -8,6 +8,42 @@ from flask import (
     flash
 )
 
+@app.route("/event/<id>/tickets")
+@users.requires_signin
+def tickets(id):
+    if not events.does_user_own_event(session["user_id"], id):
+        flash("not_own_event")
+        return redirect("/")
+    return render_template(
+        "event_tickets.html",
+        event=events.get_event_info(id),
+        tickets=events.get_ticket_list(id)
+    )
+
+@app.route("/event/<id>/passes")
+@users.requires_signin
+def passes(id):
+    if not events.does_user_own_event(session["user_id"], id):
+        flash("not_own_event")
+        return redirect("/")
+    return render_template(
+        "event_passes.html",
+        event=events.get_event_info(id),
+        passes=events.get_pass_list(id)
+    )
+
+@app.route("/event/<id>/organisers")
+@users.requires_signin
+def organisers(id):
+    if not events.does_user_own_event(session["user_id"], id):
+        flash("not_own_event")
+        return redirect("/")
+    return render_template(
+        "event_organisers.html",
+        event=events.get_event_info(id),
+        organisers=events.get_organiser_list(id)
+    )
+
 @app.route("/event/<id>", methods=["GET", "POST"])
 @users.requires_signin
 def event(id):
@@ -26,7 +62,7 @@ def event(id):
             request.form["extra-info"],
             request.form["date"]
         )
-        return redirect("/")
+        return redirect(f"/event/{id}")
 
 @app.route("/event", methods=["GET", "POST"])
 @users.requires_signin
