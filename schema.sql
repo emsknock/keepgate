7427a10bd709c93280fa590e1d647c4c3ab6599a
emsknock <25252659+emsknock@users.noreply.github.com>
2021-10-24 00:07:56 +0200

    Fix wrong use of on delete cascade and syntax

--- schema.sql
+++ schema.sql
@@ -1,22 +1,20 @@
 CREATE TABLE users (
     id          SERIAL PRIMARY KEY,
     username    TEXT UNIQUE NOT NULL,
-    passhash    TEXT NOT NULL,
-    ON DELETE CASCADE
+    passhash    TEXT NOT NULL
 );
 
 CREATE TABLE events (
     id          SERIAL PRIMARY KEY,
-    user_id     INTEGER REFERENCES users NOT NULL,
+    user_id     INTEGER REFERENCES users ON DELETE CASCADE NOT NULL,
     title       TEXT NOT NULL, -- Title of the event shown on all tickets
     extra_info  TEXT, -- Extra info shown on all tickets
     date        TIMESTAMP WITH TIME ZONE -- Date shown on all tickets,
-    ON DELETE CASCADE
 );
 
 CREATE TABLE tickets (
     id          UUID PRIMARY KEY,
-    event_id    INTEGER REFERENCES events NOT NULL,
+    event_id    INTEGER REFERENCES events ON DELETE CASCADE NOT NULL,
     user_id     INTEGER REFERENCES users, -- Only set if requires login to display
     extra_info  TEXT, -- Extra info shown only for specific ticket
     stamped     BOOLEAN NOT NULL DEFAULT FALSE, -- Set true on first scan
@@ -27,18 +25,17 @@ CREATE TABLE tickets (
 
 CREATE TABLE passes (
     id          UUID PRIMARY KEY,
-    event_id    INTEGER REFERENCES events NOT NULL,
+    event_id    INTEGER REFERENCES events ON DELETE CASCADE NOT NULL,
     user_id     INTEGER REFERENCES users, -- Only set if requires login to display
     value       INTEGER DEFAULT 0,
     extra_info  TEXT,
-    created_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    ON DELETE CASCADE
+    created_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE organisers (
     id          SERIAL PRIMARY KEY,
-    event_id    INTEGER REFERENCES events,
-    user_id     INTEGER REFERENCES users,
+    event_id    INTEGER REFERENCES events ON DELETE CASCADE,
+    user_id     INTEGER REFERENCES users ON DELETE CASCADE,
     UNIQUE (event_id, user_id),
     --can_create  BOOLEAN, -- Can create tickets and passes
     --can_delete  BOOLEAN, -- Can remove tickets and passes
@@ -50,8 +47,8 @@ CREATE TABLE organisers (
 
 CREATE TABLE pass_transactions (
     id          SERIAL PRIMARY KEY,
-    pass_id     UUID REFERENCES passes,
-    user_id     INTEGER REFERENCES users,
+    pass_id     UUID REFERENCES passes ON DELETE CASCADE,
+    user_id     INTEGER REFERENCES users ON DELETE CASCADE,
     value       INTEGER,
     time        TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
